fb41e8dc34ef14fd6ca35ad12e587edecde5d5a8
Dariusz Garbarz <d.garbarz@ela.pl>
2024-10-02 10:24:12 +0200

    fix: cors policy not defined

--- BleBoxAirSensorSim/AirSensorSimEntryPoint.cs
+++ BleBoxAirSensorSim/AirSensorSimEntryPoint.cs
@@ -7,8 +7,20 @@ builder.Services.AddControllers();
 builder.Services.AddSingleton<IDeviceStateService, DeviceStateService>();
 builder.Services.AddSingleton<IAirSensorSettingsService, AirSensorSettingsService>();
 
+var AllowAllPolicy = "_allowAllPolicy";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowAllPolicy,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                      });
+});
+
 var app = builder.Build();
 app.MapControllers();
+app.UseCors(AllowAllPolicy);
 
 var airSensorSettingsService = app.Services.GetRequiredService<IAirSensorSettingsService>();
 
